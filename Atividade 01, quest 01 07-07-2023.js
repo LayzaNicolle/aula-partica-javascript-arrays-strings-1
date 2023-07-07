5bb96d9cc3c5e1997813bbe4e21d433821db540d
Layza Nicolle <129307127+LayzaNicolle@users.noreply.github.com>
2023-07-07 18:03:06 -0300

    Atividade 01, quest 01 Tquarta - 07/07/2023

---
+++ Atividade 01, quest 01 07-07-2023.js
@@ -0,0 +1,4 @@
+var dominioA = "mundojs";
+var dominioB = 'google';
+var result = dominioA + dominioB + ' Olá, mundojs. O site www.google.com te ajuda a achar muitos materiais de estudo.';
+console.log(result);
