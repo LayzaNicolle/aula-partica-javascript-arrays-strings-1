759b7aa4eb7a69f886c238f2ac011e9bdf46c593
Layza Nicolle <129307127+LayzaNicolle@users.noreply.github.com>
2023-07-07 18:03:39 -0300

    Atividade 01, quest 02 Tquarta - 07/07/2023

---
+++ Atividade 01, quest 02 07-07-2023.js
@@ -0,0 +1,14 @@
+var valorA = "casa";
+var valorB = "asa";
+
+console.log(valorA);
+console.log(valorB);
+
+var result = valorA.toUpperCase();
+var resultB = valorB.toUpperCase();
+
+console.log(result);
+console.log(resultB);
+
+var comparacao = valorB.localeCompare(valorB);
+console.log(comparacao);
